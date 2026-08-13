eda03f245a0b5b9ad481019b92f7b79e11cfd518
Shahab <cheebuy333@gmail.com>
2026-08-14 00:02:24 +0330

    7th commit: change product_detail.html and views.py

--- eshop/views.py
+++ eshop/views.py
@@ -4,19 +4,13 @@ from eshop.models import Product
 
 def product_list(request):
     products = Product.objects.all()
-    number_of_products = products.count()
 
     return render(request, 'eshop/product_list.html', {
         'products': products,
-        'number_of_products': number_of_products,
     })
 
 
 def product_detail(request, slug):
-    # try:
-    #     product = Product.objects.get(slug=slug)
-    # except:
-    #     raise Http404
     product = get_object_or_404(Product, slug=slug)
     return render(request, 'eshop/product_detail.html', {
         'product': product,
