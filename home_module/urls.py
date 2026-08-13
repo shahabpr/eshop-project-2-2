f756ef6918607c7eea164305d9384f7df78f6c25
Shahab <cheebuy333@gmail.com>
2026-08-13 19:15:26 +0330

    6th Commit:  review all files and some changes

--- home_module/urls.py
+++ home_module/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('', views.home_page, name='home_page'),
     path('contact-us', views.contact_page, name='contact_page'),
     path('about-us', views.about_page, name='about_page'),
-    # path('site-header', views.site_header_partial, name='site_header_partial'),
 ]
